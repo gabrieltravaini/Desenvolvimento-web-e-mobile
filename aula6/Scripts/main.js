cdccdebcf183747f6703f63d81ff218dbf5f7658
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-09-30 21:11:47 -0300

    finished aula 6

--- aula6/Scripts/main.js
+++ aula6/Scripts/main.js
@@ -1,4 +1,19 @@
+import obj from "./module.js"
 
+obj.fazAlgo();
 
+let height = 1.80
 
-fazAlgo();
+let person= {
+    name:"John",
+    age:20,
+    height,
+    b_day (){ this.age++}
+}
+person.b_day();
+console.log('person', person)
+
+let name= person.name;
+let age=person.age;
+
+console.log("saida",{name, age})
--- aula6/Scripts/module.js
+++ aula6/Scripts/module.js
@@ -1,3 +1,9 @@
 function fazAlgo(){
     console.log("fiz algo!"); 
 }
+
+function fazOutraCoisa(){
+    console.log('fiz outra coisa!')
+}
+
+export default {fazAlgo, fazOutraCoisa};
