e73ade1c7983f79d2853f45b8da263156d522a33
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-10-14 21:50:28 -0300

    dadwadwad

--- aula8/src/App.jsx
+++ aula8/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-lg">Lista de frutas</h1>
+      <h1 className="text-xl">Lista de frutas</h1>
       <ListaFrutas/>
     </div>
   )
